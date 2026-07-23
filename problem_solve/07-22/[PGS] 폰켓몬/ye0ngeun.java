0bd1b8771d2b13264a19e936ab5b591a2e3a91c1
ye0ngeun <121778825+ye0ngeun@users.noreply.github.com>
2026-07-23 14:20:20 +0900

    [PGS] 폰켓몬

--- problem_solve/07-22/[PGS] 폰켓몬/ye0ngeun.java
+++ problem_solve/07-22/[PGS] 폰켓몬/ye0ngeun.java
@@ -1,10 +1,19 @@
-import java.io.*;
-import java.util.*;
-
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(
-            new InputStreamReader(System.in)
-        );
+class Solution {
+    public int solution(int[] numbers, int target) {
+        return dfs(numbers, target, 0, 0);
+    }
+    
+    public int dfs(int[] numbers, int target, int index, int total) {
+        if (index == numbers.length) {
+            if (total == target) {
+                return 1;
+            }
+            return 0;
+        }
+        
+        int plus = dfs(numbers, target, index + 1, total + numbers[index]);
+        int minus = dfs(numbers, target, index + 1, total - numbers[index]);
+        
+        return plus + minus;
     }
 }
