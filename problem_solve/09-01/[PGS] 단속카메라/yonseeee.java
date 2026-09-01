b31114728ada948312c5db48b529e4032034d8ab
Seyeon Kim <102506114+yonseeee@users.noreply.github.com>
2026-09-01 14:47:15 +0900

    Solve: [PGS] - 단속 카메라

--- problem_solve/09-01/[PGS] 단속카메라/yonseeee.java
+++ problem_solve/09-01/[PGS] 단속카메라/yonseeee.java
@@ -1,8 +1,24 @@
-import java.io.*;
 import java.util.*;
 
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+class Solution {
+    public int solution(int[][] routes) {
+        int answer = 0;
+        int n=routes.length;
+        boolean[]used=new boolean[n];
+        
+        Arrays.sort(routes, (a,b)->a[1]-b[1]);
+
+        
+        for(int i=0;i<n;i++){
+            int right=routes[i][1];
+            
+            int j;
+            for(j=i+1;j<n;j++){
+                if(!(right>=routes[j][0]&&right<=routes[j][1]))break;
+            }
+            i=j-1;
+            answer++;
+        }
+        return answer;
     }
 }
