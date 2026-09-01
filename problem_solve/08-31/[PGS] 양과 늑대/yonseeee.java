5d7962fff36636e0c50e468ae99d323b6fd2e398
Seyeon Kim <102506114+yonseeee@users.noreply.github.com>
2026-09-01 09:18:51 +0900

    Solve: [PGS] - 양과 늑대
    - DFS + 백트래킹으로 방문 가능한 모든 경우 탐색
    - 현재까지 방문한 노드의 자식 중 방문하지 않은 노드를 다음 탐색 후보로 선택
    - 노드 방문 시 양/늑대 수를 갱신하고, 늑대 >= 양이면 가지치기
    - 탐색 후 방문 상태를 복구하며 가능한 경로를 모두 확인하고 최대 양의 수 계산

--- problem_solve/08-31/[PGS] 양과 늑대/yonseeee.java
+++ problem_solve/08-31/[PGS] 양과 늑대/yonseeee.java
@@ -1,8 +1,40 @@
-import java.io.*;
 import java.util.*;
 
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+class Solution {
+    public int solution(int[] info, int[][] edges) {
+        int answer = 0;
+        
+        boolean[] visited=new boolean[info.length];
+        visited[0]=true;
+        
+        return dfs(info, edges, visited, 0, 1);
+    }
+    
+    private int dfs(int[] info, int[][] edges, boolean[] visited, int wolf, int sheep){
+        if(wolf>=sheep){
+            return sheep;
+        }
+        
+        int max=sheep;
+        
+        for(int[] edge: edges){
+            int parent=edge[0];
+            int child=edge[1];
+            
+            if(visited[parent]&&!visited[child]){
+                visited[child]=true;
+                
+                if(info[child]==1){//늑대
+                    max=Math.max(max, dfs(info, edges, visited, wolf+1, sheep));
+                }else{//양
+                    max=Math.max(max, dfs(info, edges, visited, wolf, sheep+1));
+                }
+                
+                visited[child]=false;
+            }
+        
+        }
+        
+        return max;
     }
 }
