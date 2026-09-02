3ba7858adac0316ca6a6939424904d35e7c7ab95
Seyeon Kim <102506114+yonseeee@users.noreply.github.com>
2026-09-02 15:00:20 +0900

    Solve: [PGS] - 다단계 칫솔

--- problem_solve/09-02/[PGS] 다단계 칫솔 판매/yonseeee.java
+++ problem_solve/09-02/[PGS] 다단계 칫솔 판매/yonseeee.java
@@ -1,8 +1,38 @@
-import java.io.*;
 import java.util.*;
 
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+class Solution {
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        int[] answer = new int[enroll.length];
+        
+        Map<String, String> parent=new HashMap<>();
+        Map<String, Integer> index=new HashMap<>();
+        
+        for(int i=0;i<enroll.length;i++){
+            parent.put(enroll[i], referral[i]);
+            index.put(enroll[i], i);
+        }
+        
+        int[]earn=new int[enroll.length+1];
+        
+        for(int i=0;i<seller.length;i++){
+            
+            String current=seller[i];
+            int money=amount[i]*100;
+            
+            while(!current.equals("-")&&money>0){
+                int up=money/10;
+                int mine=money-up;
+                
+                earn[index.get(current)]+=mine;
+                
+                current=parent.get(current);
+                money=up;
+            }
+        }
+        
+        for(int i=0;i<earn.length-1;i++){
+            answer[i]=earn[i];
+        }
+        return answer;
     }
 }
