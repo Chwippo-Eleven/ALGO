d60f4e94b92081b44c72c3b22f341e43e5ccdfb1
강재준 <90443794+BBZJUN@users.noreply.github.com>
2026-09-03 11:54:11 +0900

    Update BBZJUN.java

--- problem_solve/09-03/[PGS] 점프와 순간 이동/BBZJUN.java
+++ problem_solve/09-03/[PGS] 점프와 순간 이동/BBZJUN.java
@@ -1,8 +1,19 @@
-import java.io.*;
 import java.util.*;
 
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+public class Solution {
+    public int solution(int n) {
+        int ans = 0;
+
+        // 홀수는 직전에서 +1
+        // 짝수는 그냥 전전에서 2배라 +0
+        
+        while(n>0){
+            if (n%2==1){
+                ans = ans + 1;
+            }
+            n=n/2;
+        }
+
+        return ans;
     }
 }
