1413a0a2b1916824335e56bc39c0fb7836f49671
박천익 <pcy3083@naver.com>
2026-09-03 11:24:54 +0900

    제출

--- problem_solve/09-03/[PGS] 점프와 순간 이동/qkrcjsdlr10.java
+++ problem_solve/09-03/[PGS] 점프와 순간 이동/qkrcjsdlr10.java
@@ -1,8 +1,19 @@
-import java.io.*;
 import java.util.*;
 
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+public class Solution {
+    public int solution(int n) {
+        int ans = 0;
+        while(n > 0){
+            if(n % 2 == 0){
+                n /= 2;
+                continue;
+            }else{
+                n -= 1;
+                n /= 2;
+            }
+            ans++;
+        }
+
+        return ans;
     }
 }
