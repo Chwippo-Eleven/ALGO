42bd085e97e6feecaccf96c3eb67018b6ff3b513
oneul0 <neul.dev@gmail.com>
2026-09-08 10:57:13 +0900

    Letter Combinations of a Phone Number

--- problem_solve/09-08/[LTC] Letter Combinations of a Phone Number/oneul0.java
+++ problem_solve/09-08/[LTC] Letter Combinations of a Phone Number/oneul0.java
@@ -1,8 +1,22 @@
-import java.io.*;
-import java.util.*;
-
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+class Solution {
+    String digits;
+    List<String> result = new ArrayList<>();
+    String[] strings = {" ", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    public List<String> letterCombinations(String digits) {
+        this.digits = digits;
+        comb(0, new StringBuilder());
+        return result;
+    }
+    public void comb(int depth, StringBuilder str){
+        if(depth >= digits.length()) {
+            result.add(str.toString());
+            return;
+        }
+        int cur = digits.charAt(depth)-'0';
+        for(char c : strings[cur].toCharArray()){
+            str.append(c);
+            comb(depth+1, str);
+            str.deleteCharAt(str.length()-1);
+        }
     }
 }
