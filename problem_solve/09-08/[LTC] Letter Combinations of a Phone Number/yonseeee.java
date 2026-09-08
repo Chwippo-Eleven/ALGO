4ffd17fd38acc4be184cd73ce7b5e347e7280b7a
Seyeon Kim <102506114+yonseeee@users.noreply.github.com>
2026-09-08 14:33:18 +0900

    Solve: [LTC] - Letter Combinations of a Phone Number

--- problem_solve/09-08/[LTC] Letter Combinations of a Phone Number/yonseeee.java
+++ problem_solve/09-08/[LTC] Letter Combinations of a Phone Number/yonseeee.java
@@ -1,8 +1,27 @@
-import java.io.*;
-import java.util.*;
+class Solution {
+    private List<String> answer;
+    public List<String> letterCombinations(String digits) {
+        
+        answer=new ArrayList<>();
+        char[][] arr=new char[10][];
 
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char ch='a';
+        for(int i=2;i<=9;i++){
+            if(i==7||i==9)arr[i]= new char[]{ch++, ch++, ch++, ch++};
+            else arr[i]=new char[]{ch++, ch++, ch++};
+        }
+        dfs(digits, 0, "", arr);
+        return answer;
+    }
+
+    private void dfs(String digits, int idx, String current, char[][] arr){
+        if(idx==digits.length()){
+            answer.add(current);
+            return;
+        }
+        int num=digits.charAt(idx)-'0';
+        for(int i=0;i<arr[num].length;i++){
+            dfs(digits, idx+1, current+arr[num][i], arr);
+        }
     }
 }
