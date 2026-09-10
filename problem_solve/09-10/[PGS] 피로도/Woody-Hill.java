293565de5101f993fd2e5d29c86c26a91ed9fe12
Woody-Hill <150872127+Woody-Hill@users.noreply.github.com>
2026-09-10 18:08:37 +0900

    solve: [PGS] 피로도

--- problem_solve/09-10/[PGS] 피로도/Woody-Hill.java
+++ problem_solve/09-10/[PGS] 피로도/Woody-Hill.java
@@ -1,8 +1,34 @@
-import java.io.*;
-import java.util.*;
-
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+class Solution {
+    
+    int[][] dungeons;
+    boolean[] visited;
+    
+    int n;
+    int maxVisit;
+    
+    public int solution(int k, int[][] dungeons) {
+        n = dungeons.length;
+        maxVisit = 0;
+        
+        this.dungeons = dungeons;
+        visited = new boolean[n];
+        
+        explore(0, k);
+        return maxVisit;
+    }
+    
+    void explore(int cnt, int energy) {
+        
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) continue;
+            
+            if (energy >= dungeons[i][0]) {
+                visited[i] = true;
+                explore(cnt + 1, energy - dungeons[i][1]);
+                visited[i] = false;
+            }
+        }
+        
+        maxVisit = Math.max(maxVisit, cnt);
     }
 }
