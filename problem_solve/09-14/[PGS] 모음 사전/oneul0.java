88309504e6575bdcae4d75c186b491755d3e175e
oneul0 <neul.dev@gmail.com>
2026-09-14 17:01:35 +0900

    모음 사전

--- problem_solve/09-14/[PGS] 모음 사전/oneul0.java
+++ problem_solve/09-14/[PGS] 모음 사전/oneul0.java
@@ -1,8 +1,26 @@
-import java.io.*;
 import java.util.*;
-
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+class Solution {
+    String word;
+    boolean found = false;
+    int count = 1, answer = 0;
+    char[] chars = {'A', 'E', 'I', 'O', 'U'};
+    public int solution(String word) {
+        this.word = word;
+        dfs(0, new StringBuilder());
+        return answer;
+    }
+    public void dfs(int depth, StringBuilder sb){
+        if(found || depth == 5) return;
+                
+        for(char c : chars){
+            sb.append(c);
+            if(sb.toString().equals(word)){
+                found = true;
+                answer = count;
+            }
+            count++;
+            dfs(depth+1, sb);
+            sb.deleteCharAt(sb.length()-1);
+        }
     }
 }
