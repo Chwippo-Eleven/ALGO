17b2589eccd32837ee30b075eee20de75207966a
Woody-Hill <150872127+Woody-Hill@users.noreply.github.com>
2026-09-15 08:32:30 +0900

    solve:  [PGS] 모음 사전

--- problem_solve/09-14/[PGS] 모음 사전/Woody-Hill.java
+++ problem_solve/09-14/[PGS] 모음 사전/Woody-Hill.java
@@ -1,8 +1,30 @@
-import java.io.*;
-import java.util.*;
-
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+class Solution {
+    public int solution(String word) {
+        
+        char[] letters = {'A', 'E', 'I', 'O', 'U'};
+        
+        int n = letters.length;
+        
+        int[] offset = new int[n + 1];
+        
+        for (int i = 4; i >= 0; i--) {
+            offset[i] = 1 + 5 * offset[i + 1];
+        }
+        
+        int wordOrder = 0;
+        
+        OuterLoop:
+        for (int idx = 0; idx < word.length(); idx++) {
+            char ch = word.charAt(idx);
+            
+            for (int ord = 0; ord < n; ord++) {
+                if (letters[ord] == ch) {
+                    wordOrder += 1 + ord * offset[idx];
+                    continue OuterLoop;
+                }
+            }
+        }
+        
+        return wordOrder;
     }
 }
