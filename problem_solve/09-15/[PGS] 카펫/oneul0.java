4c7dcbff331fca05079785f4357f64a91d410d31
oneul0 <neul.dev@gmail.com>
2026-09-15 22:44:17 +0900

    카펫

--- problem_solve/09-15/[PGS] 카펫/oneul0.java
+++ problem_solve/09-15/[PGS] 카펫/oneul0.java
@@ -1,8 +1,25 @@
-import java.io.*;
-import java.util.*;
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
 
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int area = brown+yellow;
+
+        //노랑이 존재하기 위한 최소 높이와 너비
+        for(int i = 3; i<=area; i++){
+            int c = i;
+            int r = area/c;
+
+            if(r < 3) continue;
+
+            if(r >= c){
+                if((r-2)*(c-2) == yellow){
+                    answer[0] = r;
+                    answer[1] = c;
+                    break;
+                }
+            }
+        }
+
+        return answer;
     }
 }
