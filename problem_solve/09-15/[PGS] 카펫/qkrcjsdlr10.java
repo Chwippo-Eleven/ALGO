90435880841b58d15a24719c4dd25b46ab1e3a18
박천익 <pcy3083@naver.com>
2026-09-16 10:20:52 +0900

    데일리 - 1문제

--- problem_solve/09-15/[PGS] 카펫/qkrcjsdlr10.java
+++ problem_solve/09-15/[PGS] 카펫/qkrcjsdlr10.java
@@ -1,8 +1,17 @@
-import java.io.*;
-import java.util.*;
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int [2];
+        int sum = brown / 2 + 2;      // w + h
+        int mul = brown + yellow;     // w * h
 
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int d = sum * sum - 4 * mul;
+
+        int w = (sum + (int)Math.sqrt(d)) / 2;
+        int h = (sum - (int)Math.sqrt(d)) / 2;
+        
+        answer[0] = w;
+        answer[1] = h;
+        
+        return answer;
     }
 }
