87e473118ba24755a86c4b4d2ca0ccfad4a010ed
oneul0 <neul.dev@gmail.com>
2026-09-22 09:40:29 +0900

    Redundant Connection

--- problem_solve/09-22/[LTC] Redundant Connection/oneul0.java
+++ problem_solve/09-22/[LTC] Redundant Connection/oneul0.java
@@ -1,8 +1,26 @@
-import java.io.*;
-import java.util.*;
+class Solution {
+    int[] parent;
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        parent = new int[n+1];
+        for(int i = 0; i<=n; i++){
+            parent[i] = i;
+        }
 
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for(int[] edge : edges){
+            if(find(edge[0]) == find(edge[1])) return edge;
+            union(edge[0], edge[1]);
+        }
+        return new int[2];
+    }
+    public int find(int a){
+        if(a == parent[a]) return a;
+        return parent[a] = find(parent[a]);
+    }
+    public void union(int a, int b){
+        a = find(a);
+        b = find(b);
+        if(a<b) parent[b] = a;
+        else parent[a] = b;
     }
 }
